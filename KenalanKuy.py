8a436c9e0b050aac36179bf6d1ef8b3d2db33c9c
MRF1TR4/404 <58329775+FITRAKUN404@users.noreply.github.com>
2019-12-11 14:17:21 +0700

    MAU-NGERECORD-YAH-KONTOL.sh
    MANA ada isinya Goblok
    Makanya Jangan Record Mulu Anjing
    Subscribe channel admin => JEJEAK TUTORIAL ID

---
+++ KenalanKuy.py
@@ -0,0 +1,47 @@
+import time
+import sys
+
+def jam(s):
+  for c in s + '\n':
+   sys.stdout.write(c)
+   sys.stdout.flush()
+   time.sleep(1./10)
+
+
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+curl http://wttr.in/ [  ]
+cowsay -f eyes Welcome To MR.F1TR4 ID hackED | lolcat
+toilet -f standard " MR.F1TR4 ID " -F gay
+neofetch
+echo " Time : " $TIME | lolcat
+date | lolcat
+echo  Username : MR.F1TR4 CYBER TEAM| lolcat
+echo  Hostname : @MR.F1TR4 ID IS HACKED  | lolcat
+
+figlet MR.F1TR4 CYBER ARMY | lolcat
+figlet TEMPAT PERKENALAN    | lolcat
+
+echo ###          ###
+echo ####     ####
+echo ##########
+echo ##  ###     ##
+echo ##              ## 
+echo ##              ## • 🇨 • 🇦 
+echo ##              ##
+echo ##              ##
+
+echo 🇵 🇪 🇷 🇰 🇪 🇳 🇦 🇱 🇦 🇳 
+
+jam ('TEAM CYBER : MR.F1TR4 CYBER ARMY '))
+jam('ORANG BAIK BERAWAL DARI ORANG JAHAT YANG DI SEPELEKAN')
+jam('sfx: Lay Lay Lay Panggil Aku Si Jablay:v')
+jam('Text Berjalan Ye Gaes')
+jam('Jangan Lupa SubsBrekk:v')
---
+++ ToolsInstallerMR-F1TR4.sh
@@ -0,0 +1,162 @@
+#!/system/xbin/bash
+#NGAPAI GAN...MAU DI RECORD YAH...
+#BERARTI LU LEBIH GOBLOK/BODOH DARI PADA ORANG GILA DI LUAR SANA
+#DAN APA JUGA GUNANYA NANO COBA,ORANG BUAT SCRIPT CAPE CAPE,LU ENAKNYA AJA NGERECOD
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "T00Ls" | lolcat
+toilet -f big -F gay "FITRAKUN" 
+echo ""      
+echo "\033[32;1mAuthor : Mr.F1TR4 Feat Mr.FITRAKUN" 
+echo "\033[35;1mTeam : MR.F1TR4 CYBER TEAM"
+echo "\033[33;1mKontak Me :  088262147113"
+echo "\033[31;1mSubcribe Youtube:JEJAK TUTORIAL ID"
+echo "\033[33;1mKING GANJA NOOB" "\033[32;1m&" "\033[36;1mIndonesia Security Lite"
+echo "\033[34;1m[============================[>"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> HACK FB TARGET TOT"
+echo "[===============================================]>"
+echo $purple "2.> HACK CCTV PART 2 KE PART 1"
+echo "[===============================================]>"
+echo $cyan "3.> DEPES WEBSAIT"
+echo "[===============================================]>"
+echo $red "4.> TOOLS WEEMAN"
+echo "[===============================================]>"
+echo $white "5.> TOOLS RED-HAWK"
+echo "[===============================================]>"
+echo $green "6.> KENALAN DULU EKAN :V "
+echo "[===============================================]>"
+echo $purple "7.> TOOls BLACKeye"
+echo "[===============================================]>"
+echo $cyan "8.> TOOLS SHELLPHIS"
+echo "[===============================================]>"
+echo $red "9.> TOOLS D-TECH"
+echo "[===============================================]>"
+echo $white "10.> TOOLS PENCARI PACAR :V"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[===============================================]>"
+echo "\033[32;1m"
+read -p "[root@T00Ls-MR.F1TR4:~#>" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "MR.F1TR4 CYBER TEAM" | lolcat
+git clone https://github.com/storiku/fbtarget
+cd fbtarget
+python2 target.py
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "FITRA KUN SUBREK CYBER TEAM"
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "AKU SAYANG KAMU :V"
+git clone https://github.com/tukangiseng/TebasIndex
+cd TebasIndex
+cd Tebas
+sh Tebas.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "MR.F1TR4 NOOB"
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "HACKERS => FITRA KUN :V"
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "KENALAN NAMA GUA RAHMAD FITRA KUN...GUA ORANGNYA GANTENG...."
+exit
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mr.fitra"
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.FITRA"
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "GUA CAPEK...:V"
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+pip2 install requests
+python2 d-tect.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "TOOLS CARI PACAR"
+figlet -f gay "TAPI BO'ONG"
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mMR.F1TR4 CYBER TEAM"
+sleep 1
+echo "\033[33;1mSEMUA ORANG BISA MENJADI HACKER ASAL KALIAN NIAT"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "SUBSCRIBE CHANEL YOUTUBE SAYA : JEJAK TUTORIAL ID"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
